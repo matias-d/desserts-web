7f807e33fa21c65e2102d3a917fe9458fb1ddbd0
matias-d <matiasc.02.2003@gmail.com>
2024-07-31 18:39:25 -0300

    fix: :construction_worker: localStorage validate window

--- utils/reducer.ts
+++ utils/reducer.ts
@@ -1,31 +1,36 @@
 import { DessertI } from "@/types/Dessert";
 
-export const cartInitialState: DessertI[] = JSON.parse(localStorage.getItem('cart') || '[]')
+export const cartInitialState: DessertI[] = typeof window !== 'undefined' 
+    ? JSON.parse(localStorage.getItem('cart') || '[]')
+    : [];
 
-type optionTypes = 'ADD_TO_CART' | 'REMOVE_TO_CART' | 'NEW_ORDER' | 'INCREMENT_PRODUCT' | 'DECREMENT_PRODUCT'
+type optionTypes = 'ADD_TO_CART' | 'REMOVE_TO_CART' | 'NEW_ORDER' | 'INCREMENT_PRODUCT' | 'DECREMENT_PRODUCT';
+
 interface Action {
-    type: optionTypes
-    payload?: DessertI
+    type: optionTypes;
+    payload?: DessertI;
 }
 
 export const updateLocalStorage = (state: DessertI[]) => {
-    localStorage.setItem('cart', JSON.stringify(state))
+    if (typeof window !== 'undefined') {
+        localStorage.setItem('cart', JSON.stringify(state));
+    }
 }
 
-type functionUpdate = (state: DessertI[], action: Action) => DessertI[]
+type functionUpdate = (state: DessertI[], action: Action) => DessertI[];
 
 interface UpdateStateI {
-    'ADD_TO_CART': functionUpdate,
-    'REMOVE_TO_CART': functionUpdate,
-    'NEW_ORDER': functionUpdate
-    'INCREMENT_PRODUCT' : functionUpdate
-    'DECREMENT_PRODUCT' : functionUpdate
+    'ADD_TO_CART': functionUpdate;
+    'REMOVE_TO_CART': functionUpdate;
+    'NEW_ORDER': functionUpdate;
+    'INCREMENT_PRODUCT': functionUpdate;
+    'DECREMENT_PRODUCT': functionUpdate;
 }
 
 const UPDATE_STATE_BY_ACTION: UpdateStateI = {
     ADD_TO_CART: (state, action) => {
         if (!action.payload || !action.payload.id) {
-            throw new Error("Payload or id is missing in ADD_TO_CART action")
+            throw new Error("Payload or id is missing in ADD_TO_CART action");
         }
 
         const newState = [
@@ -34,75 +39,74 @@ const UPDATE_STATE_BY_ACTION: UpdateStateI = {
                 ...action.payload,
                 quantity: 1
             }
-        ]
+        ];
 
-        updateLocalStorage(newState)
+        updateLocalStorage(newState);
 
-        return newState
+        return newState;
     },
     REMOVE_TO_CART: (state, action) => {
         if (!action.payload || !action.payload.id) {
-            throw new Error("Payload or id is missing in REMOVE_TO_CART action")
+            throw new Error("Payload or id is missing in REMOVE_TO_CART action");
         }
 
-        const { id } = action.payload
-        const newState = state.filter(item => item.id !== id)
-        updateLocalStorage(newState)
-        return newState
+        const { id } = action.payload;
+        const newState = state.filter(item => item.id !== id);
+        updateLocalStorage(newState);
+        return newState;
     },
     NEW_ORDER: () => {
-        updateLocalStorage([])
-        return []
+        updateLocalStorage([]);
+        return [];
     },
-    DECREMENT_PRODUCT : (state, action) => {
+    DECREMENT_PRODUCT: (state, action) => {
         if (!action.payload || !action.payload.id) {
-            throw new Error("Payload or id is missing in INCREMENT_PRODUCT action")
+            throw new Error("Payload or id is missing in DECREMENT_PRODUCT action");
         }
 
-        const { id } = action.payload
+        const { id } = action.payload;
 
-        const dessertInCartIndex = state.findIndex(item => item.id === id)
+        const dessertInCartIndex = state.findIndex(item => item.id === id);
 
-        if (dessertInCartIndex < 0) return state
+        if (dessertInCartIndex < 0) return state;
 
         if (state[dessertInCartIndex].quantity === 1) {
-            const newState = state.filter(s => s.id !== id)
-            updateLocalStorage(newState)
-            return newState
+            const newState = state.filter(s => s.id !== id);
+            updateLocalStorage(newState);
+            return newState;
         }
 
         const newState = [
             ...state.slice(0, dessertInCartIndex),
             { ...state[dessertInCartIndex], quantity: state[dessertInCartIndex].quantity! - 1 },
             ...state.slice(dessertInCartIndex + 1)
-        ]
-        updateLocalStorage(newState)
-        return newState
+        ];
+        updateLocalStorage(newState);
+        return newState;
     },
-    INCREMENT_PRODUCT : (state, action) => {
-
+    INCREMENT_PRODUCT: (state, action) => {
         if (!action.payload || !action.payload.id) {
-            throw new Error("Payload or id is missing in INCREMENT_PRODUCT action")
+            throw new Error("Payload or id is missing in INCREMENT_PRODUCT action");
         }
 
-        const { id } = action.payload
+        const { id } = action.payload;
 
-        const dessertInCartIndex = state.findIndex(item => item.id === id)
+        const dessertInCartIndex = state.findIndex(item => item.id === id);
 
-        if (dessertInCartIndex < 0) return state
+        if (dessertInCartIndex < 0) return state;
 
         const newState = [
             ...state.slice(0, dessertInCartIndex),
             { ...state[dessertInCartIndex], quantity: state[dessertInCartIndex].quantity! + 1 },
             ...state.slice(dessertInCartIndex + 1)
-        ]
-        updateLocalStorage(newState)
-        return newState
+        ];
+        updateLocalStorage(newState);
+        return newState;
     }
 }
 
 export function cartReducer(state: DessertI[], action: Action) {
-    const { type } = action
-    const updateState = UPDATE_STATE_BY_ACTION[type]
-    return updateState ? updateState(state, action) : state
+    const { type } = action;
+    const updateState = UPDATE_STATE_BY_ACTION[type];
+    return updateState ? updateState(state, action) : state;
 }
